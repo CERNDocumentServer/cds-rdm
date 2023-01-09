1d1e54e3609248f487bdb7e9cc4159e43a885530
pablo <pineirin@gmail.com>
2023-01-09 14:40:08 +0100

    Records list: refactor override to use only jsx
    * hide section on empty results
    * closes: inveniosoftware/invenio-app-rdm#1996

--- assets/js/components/frontpage/overrides/RecordsList.js
+++ assets/js/components/frontpage/overrides/RecordsList.js
@@ -7,9 +7,9 @@
 import _get from "lodash/get";
 import _truncate from "lodash/truncate";
 import _upperCase from "lodash/upperCase";
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { withCancel, http } from "react-invenio-forms";
+import isEmpty from "lodash/isEmpty";
 import {
   Loader,
   Container,
@@ -18,60 +18,10 @@ import {
   Button,
   Message,
 } from "semantic-ui-react";
-import { RecordsResultsListItem } from "./RecordsResultsListItem";
 
-export class RecordsList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: { hits: [] },
-      isLoading: false,
-      error: null,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  componentWillUnmount() {
-    this.cancellableFetch && this.cancellableFetch.cancel();
-  }
-
-  fetchData = async () => {
-    const { fetchUrl } = this.props;
-    this.setState({ isLoading: true });
-
-    this.cancellableFetch = withCancel(
-      http.get(fetchUrl, {
-        headers: {
-          Accept: "application/vnd.inveniordm.v1+json",
-        },
-      })
-    );
-
-    try {
-      const response = await this.cancellableFetch.promise;
-      this.setState({ data: response.data.hits, isLoading: false });
-    } catch (error) {
-      console.error(error);
-      this.setState({
-        error: "Unable to load records",
-        isLoading: false,
-      });
-    }
-  };
-
-  render() {
-    const { isLoading, data, error } = this.state;
-    const { title } = this.props;
-
-    const listItems = data.hits?.map((record) => {
-      return <RecordsResultsListItem result={record} key={record.id} />;
-    });
-
-    return (
+export const CDSRecordsList = ({ isLoading, error, title, listItems }) => {
+  return (
+    !isEmpty(listItems) && (
       <>
         <Container>
           {isLoading && <Loader active inline="centered" />}
@@ -92,11 +42,13 @@ export class RecordsList extends Component {
         )}
         {error && <Message content={error} error icon="info" />}
       </>
-    );
-  }
-}
+    )
+  );
+};
 
-RecordsList.propTypes = {
+CDSRecordsList.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  fetchUrl: PropTypes.string.isRequired,
+  listItems: PropTypes.array.isRequired,
 };
