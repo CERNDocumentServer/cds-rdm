dca8b8ef40976c960b6e0efa704b2edc6214d8f2
Javier Romero Castro <jrcastro9515@gmail.com>
2023-01-27 17:35:42 +0100

    global: bump invenio-app-rdm to v11
    * add temporal fix to override
      frontpage JS

---
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -0,0 +1,14 @@
+import { CDSCarouselItem } from "../../components/communities_carousel/overrides/CarouselItem";
+import { CDSCommunitiesCarousel } from "../../components/communities_carousel/overrides/CommunitiesCarousel";
+import { CDSRecordsList } from "../../components/frontpage/overrides/RecordsList";
+import { CDSRecordsResultsListItem } from "../../components/frontpage/overrides/RecordsResultsListItem";
+
+
+export const overriddenComponents = {
+    "InvenioAppRdm.RecordsList.layout": CDSRecordsList,
+    "InvenioAppRdm.RecordsResultsListItem.layout": CDSRecordsResultsListItem,
+    "InvenioCommunities.CommunitiesCarousel.layout": CDSCommunitiesCarousel,
+    "InvenioCommunities.CarouselItem.layout": CDSCarouselItem,
+  };
+
+  
