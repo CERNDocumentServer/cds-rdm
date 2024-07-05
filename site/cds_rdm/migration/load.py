b3bc4a63ded0ae9562b78f775133cc60b00e278f
Javier Romero Castro <jrcastro9515@gmail.com>
2024-07-05 17:25:44 +0200

    migrator: add migration for SSPN

--- site/cds_rdm/migration/load.py
+++ site/cds_rdm/migration/load.py
@@ -11,6 +11,9 @@ from pathlib import Path
 
 from invenio_rdm_migrator.streams.records import RDMRecordCopyLoad
 
+from invenio_rdm_migrator.load.base import Load
+from invenio_rdm_records.proxies import current_rdm_records_service
+from invenio_access.permissions import system_identity
 
 class CDSRecordCopyLoad(RDMRecordCopyLoad):
     """CDSRecordCopyLoad."""
@@ -19,3 +22,31 @@ class CDSRecordCopyLoad(RDMRecordCopyLoad):
         """Constructor."""
         tmp_dir = Path(tmp_dir).absolute()
         super().__init__(communities_cache, db_uri, tmp_dir)
+
+
+class CDSRecordServiceLoad(Load):
+    """CDSRecordServiceLoad."""
+    
+    def __init__(self, db_uri, data_dir, tmp_dir, existing_data=False, entries=None):
+        """Constructor."""
+        self.db_uri = db_uri
+        self.data_dir = data_dir
+        self.tmp_dir = tmp_dir
+        self.existing_data = existing_data
+        self.entries = entries
+        
+    def _prepare(self, entry):
+        """Prepare the record"""
+        pass
+
+    def _load(self, entry):
+        """Use the services to load the entries."""
+        identity = system_identity # Should we create an idenity for the migration?
+        draft = current_rdm_records_service.create(identity, entry["record"]["json"])
+        current_rdm_records_service.publish(system_identity, draft["id"])
+    
+    def _cleanup(self, *args, **kwargs):
+        """Cleanup the entries."""
+        pass
+    
+    
