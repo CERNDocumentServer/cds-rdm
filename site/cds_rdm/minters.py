9487215184e3482bbc4233f30c4f8caa4a844c1e
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2024-09-19 15:22:49 +0200

    global: add redirection from legacy recids

---
+++ site/cds_rdm/minters.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2024 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Minters."""
+
+from invenio_pidstore.models import PersistentIdentifier, PIDStatus
+
+
+def legacy_recid_minter(legacy_recid, uuid):
+    """Legacy_recid minter."""
+    PersistentIdentifier.create(
+        pid_type="lrecid",
+        pid_value=legacy_recid,
+        object_type="rec",
+        object_uuid=uuid,
+        status=PIDStatus.REGISTERED,
+    )
