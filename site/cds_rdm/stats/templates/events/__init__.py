f7ce9994f017c4b3e76e80f460bee23be5cc12f1
zzacharo <zacharias.zacharodimos@cern.ch>
2025-01-14 11:04:45 +0100

    stats: move the description of newly added fields to init

--- site/cds_rdm/stats/templates/events/__init__.py
+++ site/cds_rdm/stats/templates/events/__init__.py
@@ -5,4 +5,13 @@
 # CDS-RDM is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
-"""Statistics events search index templates."""
+"""Statistics events search index templates.
+
+The templates were overridden to include the custom fields in the search index.
+
+Specifically, the following fields were added to the search index:
+
+- `is_lcds` (boolean): This field marks all statistical events that have been migrated from the legacy CDS system.
+- `before_COUNTER` (boolean): This field applies to all migrated events where no information was available to determine whether they were human or robot events. This was later resolved with the implementation of a proper robot-checking mechanism, ensuring COUNTER compliance.
+
+"""
