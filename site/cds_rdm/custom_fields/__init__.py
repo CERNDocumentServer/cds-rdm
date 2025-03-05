4f467a0d9abb34a72e981550163de47f51593c12
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2025-03-05 08:42:53 +0100

    custom fields: add thesis fields
    * imprint, thesis and publishing info
    * reorganise custom fields

---
+++ site/cds_rdm/custom_fields/__init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""CDS-RDM custom fields."""
+
+from cds_rdm.custom_fields.cern import CERN_CUSTOM_FIELDS, CERN_CUSTOM_FIELDS_UI
+from cds_rdm.custom_fields.publishing import PUBLISHING_FIELDS_UI
+from invenio_rdm_records.contrib.imprint import (
+    IMPRINT_CUSTOM_FIELDS,
+    IMPRINT_NAMESPACE,
+)
+from invenio_rdm_records.contrib.journal import (
+    JOURNAL_CUSTOM_FIELDS,
+    JOURNAL_NAMESPACE,
+)
+
+from invenio_rdm_records.contrib.thesis import (
+    THESIS_CUSTOM_FIELDS,
+    THESIS_NAMESPACE,
+)
+
+
+NAMESPACES = {
+    "cern": "https://greybook.cern.ch/",
+    **JOURNAL_NAMESPACE,
+    **IMPRINT_NAMESPACE,
+    **THESIS_NAMESPACE,
+}
+
+
+CUSTOM_FIELDS = [
+    *CERN_CUSTOM_FIELDS,
+    # journal
+    *JOURNAL_CUSTOM_FIELDS,
+    # imprint
+    *IMPRINT_CUSTOM_FIELDS,
+    # thesis
+    *THESIS_CUSTOM_FIELDS,
+
+]
+
+
+# Custom fields UI components
+CUSTOM_FIELDS_UI = [
+    # publishing information
+    PUBLISHING_FIELDS_UI,
+    *CERN_CUSTOM_FIELDS_UI,
+]
