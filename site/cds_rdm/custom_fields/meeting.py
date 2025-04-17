cb33454b843457869c4dc675504482177c058650
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2025-04-17 14:40:09 +0200

    meeting field: add custom config

---
+++ site/cds_rdm/custom_fields/meeting.py
@@ -0,0 +1,7 @@
+from invenio_rdm_records.contrib.meeting import (
+    MEETING_CUSTOM_FIELDS,
+    MEETING_CUSTOM_FIELDS_UI,
+    MEETING_NAMESPACE,
+)
+
+MEETING_CUSTOM_FIELDS_UI["active"] = False
--- site/cds_rdm/custom_fields/publishing.py
+++ site/cds_rdm/custom_fields/publishing.py
@@ -14,7 +14,7 @@ from invenio_rdm_records.contrib.journal import JOURNAL_CUSTOM_FIELDS_UI
 from invenio_rdm_records.contrib.thesis import THESIS_CUSTOM_FIELDS_UI
 
 PUBLISHING_FIELDS_UI = {
-    "section": _("Publishing information"),
+    "section": _("Publishing information (Imprint, Journal, Thesis)"),
     "hide_from_landing_page": False,  # hide meeting section from Additional details in landing page
     "active": False,  # collapsed by deafault
     "fields": [
