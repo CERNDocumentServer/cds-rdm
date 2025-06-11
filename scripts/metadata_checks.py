17ded62f98abafe85609761ac335a50fb2f62139
Karolina <38131488+kpsherva@users.noreply.github.com>
2025-06-11 13:31:18 +0200

    scripts: update docstring
    Co-authored-by: Javier <jrcastro9515@gmail.com>

--- scripts/metadata_checks.py
+++ scripts/metadata_checks.py
@@ -5,7 +5,7 @@
 # CDS-RDM is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Script to create metadata checks for the EC community and sub-communities.
+"""Script to create metadata checks for the CERN Research community.
 
 Usage:
 
