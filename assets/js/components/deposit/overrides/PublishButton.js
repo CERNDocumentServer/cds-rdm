df913fee30663dd0acf562971c6b8e429ee82b48
Saksham <sakshamarora1001@gmail.com>
2025-06-16 08:51:22 +0200

    assets: overrides: PublishButton: Fix handlePublish via formik

--- assets/js/components/deposit/overrides/PublishButton.js
+++ assets/js/components/deposit/overrides/PublishButton.js
@@ -1,21 +1,25 @@
+// This file is part of CDS RDM
+// Copyright (C) 2025 CERN.
+//
+// CDS RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
 import { i18next } from "@translations/invenio_rdm_records/i18next";
 import {
   PublishOrSubmitModalFormFields,
   parameters,
-} from "./SubmitReviewModalFormFields";
-import { Formik, useFormikContext } from "formik";
+} from "./SubmitReviewModal";
+import { Formik } from "formik";
 import React from "react";
-import { Checkbox, Form } from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 import PropTypes from "prop-types";
-import _get from "lodash/get";
-import _omit from "lodash/omit";
 import { Button, Icon, Message, Modal } from "semantic-ui-react";
 
 export class PublishModalComponent extends React.Component {
   render() {
     const {
       isConfirmModalOpen,
-      handleSubmit,
+      handleSubmitRecord,
       buttonLabel,
       publishWithoutCommunity,
       publishModalExtraContent,
@@ -28,54 +32,58 @@ export class PublishModalComponent extends React.Component {
     return (
       <Formik
         initialValues={formikDefaults}
-        onSubmit={handleSubmit}
+        onSubmit={(values) =>
+          handlePublish(values, handleSubmitRecord, publishWithoutCommunity)
+        }
         validationSchema={confirmSubmitReviewSchema}
         validateOnChange={false}
         validateOnBlur={false}
       >
-        <Modal
-          open={isConfirmModalOpen}
-          onClose={closeConfirmModal}
-          size="small"
-          closeIcon
-          closeOnDimmerClick={false}
-        >
-          <Modal.Header>
-            {i18next.t("Are you sure you want to publish this record?")}
-          </Modal.Header>
-          <Modal.Content>
-            <Message visible warning>
-              <p>
-                <Icon name="warning sign" />{" "}
-                {i18next.t(
-                  "Once the record is published you will no longer be able to change the files in the upload! However, you will still be able to update the record's metadata later."
+        {({ values, handleSubmit }) => {
+          return (
+            <Modal
+              open={isConfirmModalOpen}
+              onClose={closeConfirmModal}
+              size="small"
+              closeIcon
+              closeOnDimmerClick={false}
+            >
+              <Modal.Header>
+                {i18next.t("Are you sure you want to publish this record?")}
+              </Modal.Header>
+              <Modal.Content>
+                <Message visible warning>
+                  <p>
+                    <Icon name="warning sign" />{" "}
+                    {i18next.t(
+                      "Once the record is published you will no longer be able to change the files in the upload! However, you will still be able to update the record's metadata later."
+                    )}
+                  </p>
+                </Message>
+                <Form>
+                  <PublishOrSubmitModalFormFields />
+                </Form>
+                {publishModalExtraContent && (
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: publishModalExtraContent,
+                    }}
+                  />
                 )}
-              </p>
-            </Message>
-            <Form>
-              <PublishOrSubmitModalFormFields />
-            </Form>
-            {publishModalExtraContent && (
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: publishModalExtraContent,
-                }}
-              />
-            )}
-          </Modal.Content>
-          <Modal.Actions>
-            <Button onClick={closeConfirmModal} floated="left">
-              {i18next.t("Cancel")}
-            </Button>
-            <Button
-              onClick={(event) =>
-                handlePublish(event, handleSubmit, publishWithoutCommunity)
-              }
-              positive
-              content={buttonLabel}
-            />
-          </Modal.Actions>
-        </Modal>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button onClick={closeConfirmModal} floated="left">
+                  {i18next.t("Cancel")}
+                </Button>
+                <Button
+                  onClick={(event) => handleSubmit(event)}
+                  positive
+                  content={buttonLabel}
+                />
+              </Modal.Actions>
+            </Modal>
+          );
+        }}
       </Formik>
     );
   }
@@ -83,7 +91,7 @@ export class PublishModalComponent extends React.Component {
 
 PublishModalComponent.propTypes = {
   isConfirmModalOpen: PropTypes.bool.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
+  handleSubmitRecord: PropTypes.func.isRequired,
   buttonLabel: PropTypes.string,
   publishWithoutCommunity: PropTypes.bool,
   publishModalExtraContent: PropTypes.string,
--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -1,3 +1,9 @@
+// This file is part of CDS RDM
+// Copyright (C) 2025 CERN.
+//
+// CDS RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
 import { BasicCERNInformation } from "../../components/deposit/BasicInformation";
 import { CDSCarouselItem } from "../../components/communities_carousel/overrides/CarouselItem";
 import { CDSRecordsList } from "../../components/frontpage/overrides/RecordsList";
@@ -6,9 +12,9 @@ import { CDSRecordsResultsListItemDescription } from "../../components/search/ov
 import { CDSAffiliationsSuggestions } from "../../components/deposit/overrides/CDSAffiliationsSuggestions";
 import { CLCSync } from "../../components/record_details/clc_sync";
 import {
-  SubmitReviewModalFormFields,
+  SubmitReviewModalComponent,
   PublishOrSubmitModalFormFields,
-} from "../../components/deposit/overrides/SubmitReviewModalFormFields";
+} from "../../components/deposit/overrides/SubmitReviewModal";
 import { PublishModalComponent } from "../../components/deposit/overrides/PublishButton";
 
 export const overriddenComponents = {
@@ -24,8 +30,8 @@ export const overriddenComponents = {
   "InvenioAppRdm.Search.RecordsResultsListItem.description":
     CDSRecordsResultsListItemDescription,
   "InvenioAppRdm.RecordLandingPage.RecordManagement.container": CLCSync,
-  "InvenioRdmRecords.SubmitReviewModal": SubmitReviewModalFormFields,
-  "InvenioRdmRecords.SubmitReviewModal.ExtraComponents":
+  "InvenioRdmRecords.SubmitReviewModal.container": SubmitReviewModalComponent,
+  "InvenioRdmRecords.SubmitReviewModal.Form.ExtraComponent.container":
     PublishOrSubmitModalFormFields,
-  "InvenioRdmRecords.PublishModal": PublishModalComponent,
+  "InvenioRdmRecords.PublishModal.container": PublishModalComponent,
 };
