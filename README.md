bbac24c0643af67fbefa5a91a7b58b38bffd356b
Nicola <ntarocco@gmail.com>
2025-10-02 21:49:52 +0200

    feat: display indexed in INSPIRE

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 The CDS-RDM instance based on [InvenioRDM](https://inveniordm.docs.cern.ch/).
 
+## Install
+
+Make sure that you have all the [prerequisites](https://inveniordm.docs.cern.ch/install/#1-install-the-cli-tool). Then run:
+
+```
+$ invenio-cli install
+$ invenio-cli run
+```
+
+Now visit https://127.0.0.1:5000. For more detailed instructions, read the documentation [here](https://inveniordm.docs.cern.ch/install/).
+
+## Create demo users
+
+```
+pipenv run invenio users create admin@demo.org --password 123456 --active --confirm
+# grant superadmin access to a user
+pipenv run invenio access allow superuser-access user admin@demo.org
+```
+
+## Deployment
+
 ### Update dependencies before deployment
 
 To update dependencies before deployment, you need to run pipenv lock in the target deployment environment:
--- site/cds_rdm/views.py
+++ site/cds_rdm/views.py
@@ -8,10 +8,12 @@
 
 """CDS views."""
 
-from flask import Blueprint, current_app, render_template
+from flask import Blueprint, current_app, render_template, url_for
 from flask_principal import AnonymousIdentity
 from invenio_access.permissions import any_user
+from invenio_app_rdm.records_ui.utils import dump_external_resource
 from invenio_communities import current_communities
+from invenio_i18n import _
 
 blueprint = Blueprint("cds-rdm_ext", __name__)
 
@@ -38,3 +40,23 @@ def create_cds_clc_sync_bp(app):
     """Create records blueprint."""
     ext = app.extensions["cds-rdm"]
     return ext.clc_sync_resource.as_blueprint()
+
+
+def inspire_link_render(record):
+    """Entry for INSPIRE."""
+    ret = []
+    inspire_rel_ids = [
+        rel_id["identifier"]
+        for rel_id in record.data["metadata"].get("related_identifiers", [])
+        if rel_id["scheme"] == "inspire"
+    ]
+    for rel_value in inspire_rel_ids:
+        ret.append(
+            dump_external_resource(
+                f"https://inspirehep.net/literature/{rel_value}",
+                title="INSPIRE",
+                section=_("Indexed in"),
+                icon=url_for("static", filename="images/inspire_logo.png"),
+            )
+        )
+    return ret
