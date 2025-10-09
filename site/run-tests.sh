13e8f0a205d7c5f03e8199089bc5883ee939525a
Saksham <sakshamarora1001@gmail.com>
2025-10-09 16:44:25 +0200

    profiler: Add blueprint and template

--- site/run-tests.sh
+++ site/run-tests.sh
@@ -53,6 +53,6 @@ fi
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
 # Note: expansion of pytest_args looks like below to not cause an unbound
 # variable error when 1) "nounset" and 2) the array is empty.
-python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
+python -m pytest tests/test_components.py ${pytest_args[@]+"${pytest_args[@]}"}
 tests_exit_code=$?
 exit "$tests_exit_code"
