4404f8355052b44da1943173565a815448282c0d
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2026-07-09 13:05:15 +0200

    change(custom_fields): conference as an array

--- site/cds_rdm/custom_fields/meeting.py
+++ site/cds_rdm/custom_fields/meeting.py
@@ -7,11 +7,79 @@
 
 
 """CDS-RDM custom fields."""
+from functools import partial
 
 from invenio_i18n import lazy_gettext as _
 
-# keep the imports below
-from invenio_rdm_records.contrib.meeting import MEETING_CUSTOM_FIELDS, MEETING_NAMESPACE
+# attention! keep the imports below even if unused
+from invenio_rdm_records.contrib.meeting import MEETING_NAMESPACE
+from invenio_rdm_records.contrib.meeting.custom_fields import MeetingCF
+from marshmallow import fields
+from marshmallow_utils.fields import IdentifierValueSet, SanitizedUnicode
+from marshmallow_utils.schemas import IdentifierSchema
+
+from invenio_rdm_records.services.schemas.metadata import _valid_url, record_related_identifiers_schemes
+
+
+class CDSMeetingCF(MeetingCF):
+    """Nested custom field."""
+
+    @property
+    def field(self):
+        """Meeting fields definitions."""
+        return fields.List(fields.Nested(
+            {
+                "acronym": SanitizedUnicode(),
+                "dates": SanitizedUnicode(),
+                "place": SanitizedUnicode(),
+                "session_part": SanitizedUnicode(),
+                "session": SanitizedUnicode(),
+                "title": SanitizedUnicode(),
+                "url": SanitizedUnicode(
+                    validate=_valid_url(error_msg=_("You must provide a valid URL.")),
+                ),
+                "identifiers": IdentifierValueSet(
+                    fields.Nested(
+                        partial(
+                            IdentifierSchema,
+                            allowed_schemes=record_related_identifiers_schemes,
+                        )
+                    )
+                ),
+            }
+        ))
+
+    @property
+    def mapping(self):
+        """Meeting search mappings."""
+        return {
+            "type": "object",
+            "properties": {
+                "acronym": {"type": "keyword"},
+                "dates": {"type": "keyword"},
+                "place": {"type": "text"},
+                "session_part": {"type": "keyword"},
+                "session": {"type": "keyword"},
+                "title": {
+                    "type": "text",
+                    "fields": {"keyword": {"type": "keyword"}},
+                },
+                "url": {"type": "keyword"},
+                "identifiers": {
+                    "type": "object",
+                    "properties": {
+                        "identifier": {"type": "keyword"},
+                        "scheme": {"type": "keyword"},
+                    },
+                },
+            },
+        }
+
+
+MEETING_CUSTOM_FIELDS = [
+    CDSMeetingCF(name="meeting:meeting"),
+]
+
 
 MEETING_CUSTOM_FIELDS_UI = {
     "section": _("Conference"),
@@ -20,7 +88,7 @@ MEETING_CUSTOM_FIELDS_UI = {
         {
             "field": "meeting:meeting",
             "ui_widget": "CDSMeeting",
-            "template": "meeting.html",
+            "template": "CDSMeeting.html",
             "props": {
                 "title": {
                     "label": _("Title"),
