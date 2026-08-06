a1a274ca885744afc567d36d5049661dff569376
Mohammed Taha Khan <mohammed.taha.khan@cern.ch>
2026-08-06 11:56:00 +0200

    fix(search): show report numbers and EP badge in search results

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -12,7 +12,10 @@ import { BasicCERNInformation } from "../../components/deposit/BasicInformation"
 import { CDSCarouselItem } from "../../components/communities_carousel/overrides/CarouselItem";
 import { CDSRecordsList } from "../../components/frontpage/overrides/RecordsList";
 import { CDSRecordsResultsListItem } from "../../components/frontpage/overrides/RecordsResultsListItem";
-import { CDSRecordsResultsListItemDescription } from "../../components/search/overrides/CDSRecordsResultsListItemDescription";
+import {
+  CDSRecordsResultsListItemDescription,
+  CDSRecordsResultsListItemLabelsAfter,
+} from "../../components/search/overrides/CDSRecordsResultsListItemDescription";
 import { CDSAffiliationsSuggestions } from "../../components/deposit/overrides/CDSAffiliationsSuggestions";
 import { CLCSync } from "../../components/record_details/clc_sync";
 import { CommitteeApprovalManageSection } from "../../components/record_details/CommitteeApproval";
@@ -51,7 +54,13 @@ export const overriddenComponents = {
   "InvenioAppRdm.Deposit.BasicInformation.after.container": BasicCERNInformation,
   "InvenioAppRdm.Deposit.CustomFields.container": () => null,
   "ReactInvenioForms.AffiliationsSuggestions.content": CDSAffiliationsSuggestions,
+  "InvenioAppRdm.Search.RecordsResultsListItem.labels.after":
+    CDSRecordsResultsListItemLabelsAfter,
+  "InvenioCommunities.DetailsSearch.RecordsResultsListItem.labels.after":
+    CDSRecordsResultsListItemLabelsAfter,
   "InvenioAppRdm.Search.RecordsResultsListItem.description":
+    CDSRecordsResultsListItemDescription,
+  "InvenioCommunities.DetailsSearch.RecordsResultsListItem.description":
     CDSRecordsResultsListItemDescription,
   "InvenioAppRdm.RecordLandingPage.RecordManagement.container":
     RecordManagementContainer,
